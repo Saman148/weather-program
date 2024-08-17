a7f23351260cc5cf427fdd7d23305610bbc6abaf
Saman Ghasemi <127429246+Saman148@users.noreply.github.com>
2024-08-18 01:19:21 +0330

    Update test.py
    deleted api

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 city = 'london'
-api_key = 'd4c33ddd93b36361704d10744b35a7b5'
+api_key = ''
 
 res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=d4c33ddd93b36361704d10744b35a7b5').json()
 
